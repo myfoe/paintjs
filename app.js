edc07c8a3316a9899c30d275d8edfaf379f17b19
myfoe <myfoe@nate.com>
2021-04-06 22:34:55 +0900

    FInal.Version

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE)
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
-let painting = false;aaa
+let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;    
 }
 
 function startPainting() {
+    if(event.button===0){ /**마우스 왼쪽클릭일때만 그려라 */
     painting = true;
+    }
 }
 
 function onMouseMove(event) {
@@ -26,17 +40,71 @@ function onMouseMove(event) {
     }   else {
         ctx.lineTo(x, y);
         ctx.stroke();
+        }
+}
+
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(event){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "FILL";
+    } else {
+        filling =true;
+        mode.innerText = "PAINT";
     }
+    
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleCanvasClick(event){
+    if(filling){
+        ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+    
 }
 
+function handleCM(event){
+    event.preventDefault()
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "image";
+    link.click();
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
-    canvas. addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
